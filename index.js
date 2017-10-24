da911d46394117fee41a025a13469c988576b005
Derek Morey <dman6505@gmail.com>
2017-10-24 19:23:46 -0400

    Fixed bot infinite loop error, added echo post
    The bot now echos whatever is said in the chat. The bot was getting
    caught in an infinite cycle where it was talking to itself. This
    was fixed by checking the request name against the bot name.

--- index.js
+++ index.js
@@ -4,19 +4,34 @@ const request = require('request');
 
 const config = require('./config');
 
-const app = express();
 
+const app = express();
 
 app.use(bodyParser.json());
 
 app.post('/', (req, res) => {
-	if (req.body.sender_id === config.bot_id) { //don't want bot talking to itself
-		res.status(400).end()
+	if (req.body.name === config.bot_name) { //don't want bot talking to itself
+		return res.status(400).end();
 	}
-	//do stuff here
+
+
+	//simple echo bot 
+	const message = req.body.name + ' said: ' + req.body.text;
+
+	request.post({
+		url: config.url,
+		json: true,
+		body: {
+			bot_id: config.bot_id,
+			text: message,
+		}
+	}, (err, resp, body) => {
+		console.log(resp.statusCode);
+	});
+
 	res.status(200).end();
 });
 
 app.listen(config.port, config.addr, () => {
-	console.log('Listening on port: ' + config.port);
+	console.log('Listening on ' + config.addr + ':' + config.port);
 })
